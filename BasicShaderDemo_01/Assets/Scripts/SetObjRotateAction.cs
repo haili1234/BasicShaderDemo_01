9626ddd3696ef16c48aea2820a1402da80807047
haili <648854406@qq.com>
2020-11-24 15:40:16 +0800

    1、修改表面着色器对应的资源与整理 2、导入Recorder插件

---
+++ BasicShaderDemo_01/Assets/Scripts/SetObjRotateAction.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetObjRotateAction : MonoBehaviour
+{
+    public float Speed = 5;
+    public Vector3 Dir = Vector3.up;
+   
+    void Update()
+    {
+        transform.Rotate(Dir * Speed * Time.deltaTime);
+    }
+}
